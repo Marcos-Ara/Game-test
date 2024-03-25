92bdc18ec7509fde98ed10fd25e103a81fba45fb
Marcos-Ara <araujomarcos1328@gmail.com>
2024-03-25 10:29:37 -0300

    edição

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const mario = document.querySelector('.mario');
+const mario = document.querySelector('#mario')
+const pipe = document.querySelector('.pipe')
+
 
 const jump = () => {
-mario.classList.add('jump');
+    mario.classList.add('jump')
+    
 }
 
-document.addEventListener('keydown',jump);
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp' || event.key === 'w') {
+        jump()
+        setTimeout(() => {
+            mario.classList.remove('jump')
+        }, 500)
+    }
+})
 
+const loop = setinterval(() =>{
+
+const pipePosition = pipe.offsetleft;
+const marioPosition = + window.getComputedStyle(mario).bottom.replace(`px`);
+
+console.log(marioPosition);
+
+if(pipePosition <=120){
+    pipe.style.amimation = `nome`;
+    pipe.style.left = `${pipePosition} px`;
+
+}
+
+},10);
